7ee5de337f649c0f08035b03e9f237aad3d8f5d8
Thomas <thomasedwalker@gmail.com>
2020-06-24 20:31:22 -0600

    Add HSL to RGB conversion functions.

--- clrp.js
+++ clrp.js
@@ -144,6 +144,7 @@ class CLRP {
         style.innerHTML = rules;
         document.head.appendChild(style);
     }
+
 }
 
 class CLRPInput {
@@ -210,3 +211,114 @@ class CLRPInput {
         });
     }
 }
+
+////////////////////////////////
+// Color conversion functions //
+////////////////////////////////
+CLRP.hexRegex = /#([\da-fA-F]{2})([\da-fA-F]{2})([\da-fA-F]{2})/g;
+CLRP.rgbRegex = /rgb\((\d+),\s*(\d+),\s*(\d+)\)/g;
+CLRP.hslRegex = /hsl\((\d+),\s*(\d+)%,\s*(\d+)%\)/g;
+
+CLRP.color2hex = function(c) {
+    var color = CLRP.parseColor(c);
+    if (color.format == "hex") {
+        return "#" + color.r + color.g + color.b;
+    }
+    
+    if (color.format == "hsl") {
+
+    }
+};
+
+CLRP.color2hsl = function(c) {
+    var color = CLRP.parseColor(c);
+    console.log(color);
+};
+
+CLRP.color2rgb = function(c) {
+    var color = CLRP.parseColor(c);
+    if (color.format == 'hsl') {
+        var r = CLRP.hsl2rgb(color.h, color.s, color.l);
+        return `rgb(${r.r}, ${r.g}, ${r.b})`;
+    }
+    console.log(color);
+};
+
+// adapted from hsl wikipedia page
+CLRP.hsl2rgb = function(h, s, l) {
+    var sp = s / 100;
+    var lp = l / 100;
+    var chroma = (1 - Math.abs(2 * lp - 1)) * sp;
+    var x = chroma * (1 - Math.abs((h / 60) % 2 - 1));
+    var m = lp - (chroma / 2);
+    var result = {};
+    var rgb;
+
+    var hp = Math.ceil(h / 60);
+    switch(hp) {
+        case 1:
+            rgb = [chroma, x, 0];
+            break;
+        case 2:
+            rgb = [x, chroma, 0];
+            break;
+        case 3:
+            rgb = [0, chroma, x];
+            break;
+        case 4:
+            rgb = [0, x, chroma];
+            break;
+        case 5:
+            rgb = [x, 0, chroma];
+            break;
+        case 6:
+            rgb = [chroma, 0, x];
+            break;
+        default:
+            rgb = [0, 0, 0];
+    }
+
+    result.format = "rgb";
+    result.r = Math.round((m + rgb[0]) * 255);
+    result.g = Math.round((m + rgb[1]) * 255);
+    result.b = Math.round((m + rgb[2]) * 255);
+    return result;
+}
+
+CLRP.rgb2hex = function(r, g, b) {
+
+}
+
+CLRP.hex2rgb = function(hr, hg, hb) {
+
+}
+
+CLRP.parseColor = function(color) {
+    var result = {};
+    CLRP.hexRegex.lastIndex = 0;
+    CLRP.hslRegex.lastIndex = 0;
+    CLRP.rgbRegex.lastIndex = 0;
+
+    var cmatch;
+    if ((cmatch = CLRP.hexRegex.exec(color)) != null) {
+        result.format = "hex";
+        result.r = cmatch[1].toLowerCase();
+        result.g = cmatch[2].toLowerCase();
+        result.b = cmatch[3].toLowerCase();
+    } else if ((cmatch = CLRP.rgbRegex.exec(color)) != null) {
+        result.format = "rgb";
+        result.r = parseInt(cmatch[1]);
+        result.b = parseInt(cmatch[2]);
+        result.g = parseInt(cmatch[3]);
+    } else if ((cmatch = CLRP.hslRegex.exec(color)) != null) {
+        result.format = "hsl";
+        result.h = parseInt(cmatch[1]);
+        result.s = parseInt(cmatch[2]);
+        result.l = parseInt(cmatch[3]);
+    } else {
+        result.format = "unknown";
+    }
+
+    console.log(result);
+    return result;
+};
